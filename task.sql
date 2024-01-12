4bb98b0dda4bc7feb066150435399edaafd46c91
gyf <charon0707@163.com>
2024-01-12 20:56:40 +0800

    上传SQL DDL

---
+++ task.sql
@@ -0,0 +1,109 @@
+-- task.apply definition
+
+CREATE TABLE `apply` (
+  `apply_id` int NOT NULL AUTO_INCREMENT,
+  `uid` varchar(255) DEFAULT NULL,
+  `pid` varchar(255) DEFAULT NULL,
+  `match_level` float DEFAULT NULL,
+  `desire_level` float DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`apply_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- task.compitition definition
+
+CREATE TABLE `compitition` (
+  `comp_id` varchar(100) NOT NULL,
+  `title` varchar(100) DEFAULT NULL,
+  `tag` varchar(100) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  PRIMARY KEY (`comp_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- task.files definition
+
+CREATE TABLE `files` (
+  `fid` int NOT NULL,
+  `pid` varchar(100) DEFAULT NULL,
+  `file_path` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`fid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+
+-- task.group_member definition
+
+CREATE TABLE `group_member` (
+  `gid` varchar(100) NOT NULL,
+  `stuid` varchar(100) NOT NULL,
+  PRIMARY KEY (`gid`,`stuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- task.group_table definition
+
+CREATE TABLE `group_table` (
+  `gid` int unsigned NOT NULL COMMENT '自增的编码',
+  `stuid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `pid` int DEFAULT NULL,
+  `group_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `group_key` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '定长的编码'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+
+-- task.project_tag definition
+
+CREATE TABLE `project_tag` (
+  `pid` int NOT NULL,
+  `tag` varchar(10) NOT NULL,
+  PRIMARY KEY (`pid`,`tag`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- task.projects definition
+
+CREATE TABLE `projects` (
+  `pid` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `intro` varchar(5000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `gid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '关联团队',
+  `startdate` varchar(100) DEFAULT NULL,
+  `enddate` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `done` int DEFAULT NULL COMMENT '项目是否完成',
+  `mainpic` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '主图路径',
+  `requirement` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '暂不招募队友',
+  `tags` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`pid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+
+-- task.tasks definition
+
+CREATE TABLE `tasks` (
+  `tid` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `intro` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `executor_stuid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `startdate` date DEFAULT NULL,
+  `enddate` date DEFAULT NULL,
+  `done` int DEFAULT NULL,
+  `pid` int DEFAULT NULL,
+  `interface_stuid` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `importance_level` varchar(10) DEFAULT NULL COMMENT '1:非常重要 2：一般重要',
+  PRIMARY KEY (`tid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+
+
+-- task.users definition
+
+CREATE TABLE `users` (
+  `uid` int NOT NULL AUTO_INCREMENT,
+  `password` varchar(5) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `avatar` varchar(500) DEFAULT NULL,
+  `stuid` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`uid`) USING BTREE,
+  KEY `users_stuid_IDX` (`stuid`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
